417a55c25638d08fddc322528cc9b8db2f80d16a
jaymonv <47352670+jaymonv@users.noreply.github.com>
2019-02-21 14:18:55 +0100

    analysis.py

--- analysis.py
+++ analysis.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import datetime as dt
+from statsmodels.stats.proportion import proportions_ztest
 
 data = pd.read_csv('Data/DataChallenge2019_GreenhouseGroup_Bidding_Algorithms_Tests.csv',
                    sep=';').dropna(how='all')
@@ -13,18 +16,22 @@ data['inview_per_impression'] = data['impressions_inview'] / data['impressions']
 data['abovefold_per_impression'] = data['impressions_above_fold'] / data['impressions']
 data['clicks_per_impression'] = data['clicks'] / data['impressions']
 
-data['conv_per_inview'] = data['post_view_conv'] / data['impressions_inview']
-data['conv_per_click'] = data['post_click_conv'] / data['clicks']
+data['Post-view conversions per impression'] = data['post_view_conv'] / data['impressions']
+data['Post-click conversions per impression'] = data['post_click_conv'] / data['impressions']
 
-data['UU_inview_per_UU_impression'] = data['unique_user_inview'] / data['unique_user_impressions']
-data['UU_click_per_UU_impression'] = data['unique_user_is_click'] / data['unique_user_impressions']
+data['Post-view conversions per impression '] = data['Post-view conversions per impression'] / (data['buyer_bid'] / 1000)
+data['Post-click conversions per impression '] = data['Post-click conversions per impression'] / (data['buyer_bid'] / 1000)
 
-data['UU_conv_per_UU_inview'] = data['unique_user_post_view_conv'] / data['unique_user_inview']
-data['UU_conv_per_UU_click'] = data['unique_user_post_click_conv'] / data['unique_user_is_click']
+data['Unique user view per impression'] = data['unique_user_inview'] / data['impressions']
+data['Unique user click per impression'] = data['unique_user_is_click'] / data['impressions']
 
-data['inview/impression/euro'] = data['inview_per_impression'] / data['buyer_bid']
-data['clicks/impression/euro'] = data['clicks_per_impression'] / data['buyer_bid']
-data['UU_clicks/impression/euro'] = data['UU_click_per_UU_impression'] / data['buyer_bid']
+data['Unique user view per impression '] = data['unique_user_inview'] / data['impressions'] / (data['buyer_bid'] / 1000)
+data['Unique user click per impression '] = data['unique_user_is_click'] / data['impressions'] / (data['buyer_bid'] / 1000)
+data['2'] = (data['post_view_conv'] + data['post_click_conv']) / (data['buyer_bid'] / 1000)
+
+data['start_date'] = pd.to_datetime(data['start_date'])
+data['end_date'] = pd.to_datetime(data['end_date'])
+data['campaign_duration'] = (data['end_date'] - data['start_date']).dt.days
 
 def agency_AB_performance(values, data=data):
     A_mean = []
@@ -32,9 +39,11 @@ def agency_AB_performance(values, data=data):
     delta = []
     indicator = []
     rel_increase = []
+    p_value_delta = 
 
+    
     for i in values:
-        if 'euro' in i:
+        if 'impression ' in i:
             data = data[data['buyer_bid'] > 0]
     
         df = data.groupby(['campaign_group_id', 'operating_system', 'device_type', 'test_group'])[i].sum().unstack().dropna()
@@ -43,7 +52,10 @@ def agency_AB_performance(values, data=data):
         B_mean.append(np.mean(df['B']))
         delta.append(np.mean(df['B']) - np.mean(df['A']))
         rel_increase.append((np.mean(df['B']) - np.mean(df['A']))/abs(np.mean(df['A'])))
+        stat, pval = proportions_ztest(count, nobs)
 
+
+        
     performance_df = pd.DataFrame()
     performance_df['A_mean'] = A_mean
     performance_df['B_mean'] = B_mean
@@ -51,19 +63,36 @@ def agency_AB_performance(values, data=data):
     performance_df['indicator'] = indicator
     performance_df['%increase'] = rel_increase
     
+    
     return performance_df
         
-reach_performance = agency_AB_performance(values=['inview_per_impression', 
-                                           'clicks_per_impression', 
-                                           'UU_inview_per_UU_impression', 
-                                           'UU_click_per_UU_impression'])
+eff_reach_performance = agency_AB_performance(values=['Unique user click per impression'])
 
-targeting_performance = agency_AB_performance(values=['conv_per_inview',
-                                               'conv_per_click',
-                                               'UU_conv_per_UU_inview',
-                                               'UU_conv_per_UU_click'])
+sales_performance = agency_AB_performance(values=['Post-view conversions per impression',
+                                                  'Post-click conversions per impression'])
 
-efficiency_performance = agency_AB_performance(values=['inview/impression/euro',
-                                                'clicks/impression/euro',
-                                                'UU_clicks/impression/euro'])
+cost_efficiency_performance = agency_AB_performance(values=[
+                                                'Unique user view per impression '])
+
+
     
+def plot_group_barchart(dataframe, title):
+    fig, ax = plt.subplots(figsize=(6, 12))
+    ind = np.arange(len(dataframe))                                   # the x locations for the groups
+    width = 0.35                                                      # the width of the bars
+    p1 = ax.bar(ind, dataframe['A_mean'], width, color='lightblue', bottom=0)
+    p2 = ax.bar(ind + width, dataframe['B_mean'], width, color='darkblue', bottom=0)
+    #ax.set_title(title, fontsize=21)
+    ax.set_xticks(ind + width / 2)
+    ax.set_xticklabels(tuple(dataframe['indicator']), fontsize=14)
+    #ax.legend((p1[0], p2[0]), ('Control algorithm', 'Bidwiser algorith'), fontsize=15)
+    fig.savefig(fname=title)
+    
+
+#plot_group_barchart(dataframe=eff_reach_performance , title=' ')
+    
+#fig, ax = plt.subplots(figsize=(14, 8))
+plt.scatter(x=data['campaign_duration'], y=data['Unique user click per impression'])
+
+ax.set_title("Unique user click per impression per euro", fontsize=21)
+plt.xlabel("Campaign duration (days)", fontsize=15)
