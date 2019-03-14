d48e11795ba33b1809f686c29743ea622c33ce8e
jaymonv <47352670+jaymonv@users.noreply.github.com>
2019-03-14 08:58:06 +0100

    update analysis

--- analysis.py
+++ analysis.py
@@ -2,80 +2,102 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import datetime as dt
-from statsmodels.stats.proportion import proportions_ztest
+import scipy.stats as st
 
 data = pd.read_csv('Data/DataChallenge2019_GreenhouseGroup_Bidding_Algorithms_Tests.csv',
                    sep=';').dropna(how='all')
 
+data['small_pi'] = data['creative_size_small'] / data['impressions']
+data['medium_pi'] = data['creative_size_medium'] / data['impressions']
+data['large_pi'] = data['creative_size_large'] / data['impressions']
+data['inview_pi'] = data['impressions_inview'] / data['impressions']
+data['abovefold_pi'] = data['impressions_above_fold'] / data['impressions']
+data['clicks_pi'] = data['clicks'] / data['impressions']
 
-data['small_per_impression'] = data['creative_size_small'] / data['impressions']
-data['medium_per_impression'] = data['creative_size_medium'] / data['impressions']
-data['large_per_impression'] = data['creative_size_large'] / data['impressions']
-data['inview_per_impression'] = data['impressions_inview'] / data['impressions']
-data['abovefold_per_impression'] = data['impressions_above_fold'] / data['impressions']
-data['clicks_per_impression'] = data['clicks'] / data['impressions']
+data['Post-view conv pi'] = data['post_view_conv'] / data['impressions']
+data['Post-click conv pi'] = data['post_click_conv'] / data['impressions']
 
-data['Post-view conversions per impression'] = data['post_view_conv'] / data['impressions']
-data['Post-click conversions per impression'] = data['post_click_conv'] / data['impressions']
+data['Post-view conv pe'] = data['Post-view conv pi'] / (data['buyer_bid'] / 1000)
+data['Post-click conv pe'] = data['Post-click conv pi'] / (data['buyer_bid'] / 1000)
+data['Conv pi'] = (data['Post-view conv pi'] + data['Post-click conv pi']) / data['impressions']
 
-data['Post-view conversions per impression '] = data['Post-view conversions per impression'] / (data['buyer_bid'] / 1000)
-data['Post-click conversions per impression '] = data['Post-click conversions per impression'] / (data['buyer_bid'] / 1000)
+data['UU view pi'] = data['unique_user_inview'] / data['impressions']
+data['UU click pi'] = data['unique_user_is_click'] / data['impressions']
 
-data['Unique user view per impression'] = data['unique_user_inview'] / data['impressions']
-data['Unique user click per impression'] = data['unique_user_is_click'] / data['impressions']
-
-data['Unique user view per impression '] = data['unique_user_inview'] / data['impressions'] / (data['buyer_bid'] / 1000)
-data['Unique user click per impression '] = data['unique_user_is_click'] / data['impressions'] / (data['buyer_bid'] / 1000)
-data['2'] = (data['post_view_conv'] + data['post_click_conv']) / (data['buyer_bid'] / 1000)
+data['UU view pe'] = data['unique_user_inview'] / data['impressions'] / (data['buyer_bid'] / 1000)
+data['UU click pe'] = data['unique_user_is_click'] / data['impressions'] / (data['buyer_bid'] / 1000)
+data['Conv pe'] = (data['post_view_conv'] + data['post_click_conv']) / (data['buyer_bid'] / 1000)
 
 data['start_date'] = pd.to_datetime(data['start_date'])
 data['end_date'] = pd.to_datetime(data['end_date'])
 data['campaign_duration'] = (data['end_date'] - data['start_date']).dt.days
 
 def agency_AB_performance(values, data=data):
-    A_mean = []
-    B_mean = []
+
+    a_mean = []
+    b_mean = []
     delta = []
     indicator = []
     rel_increase = []
-    p_value_delta = 
-
+    p_value_delta = []
+    test_count = []
     
     for i in values:
-        if 'impression ' in i:
+        if ' pe' in i:
             data = data[data['buyer_bid'] > 0]
-    
+        global df
         df = data.groupby(['campaign_group_id', 'operating_system', 'device_type', 'test_group'])[i].sum().unstack().dropna()
+        df2 = data.groupby(['campaign_group_id', 'operating_system', 'device_type', 'test_group'])['impressions'].sum().unstack().dropna()
+        
+        df = pd.concat([df, df2], axis=1)
+        df.columns = ['A', 'B', 'A_count', 'B_count']
+        
         indicator.append(i)
-        A_mean.append(np.mean(df['A']))
-        B_mean.append(np.mean(df['B']))
+        a_mean.append(np.mean(df['A']))
+        b_mean.append(np.mean(df['B']))
         delta.append(np.mean(df['B']) - np.mean(df['A']))
         rel_increase.append((np.mean(df['B']) - np.mean(df['A']))/abs(np.mean(df['A'])))
-        stat, pval = proportions_ztest(count, nobs)
 
+        # Z-test B > A
+        x_bar = df['B']
+        q = df['A']
 
+        z = (x_bar-q) * np.sqrt((df['A_count'] + df['B_count'])/(q*(1-q)))
+        pval = 2*(1-st.norm.cdf(abs(z)))
+        count = 0
+        y=0
+        for i in pval:
+            if i < 0.05:
+                count += 1
+            y += 1
         
+        p_value_delta.append(count)
+        test_count.append(y)
+
+
     performance_df = pd.DataFrame()
-    performance_df['A_mean'] = A_mean
-    performance_df['B_mean'] = B_mean
-    performance_df['delta'] = delta
     performance_df['indicator'] = indicator
+    performance_df['A_mean'] = a_mean
+    performance_df['B_mean'] = b_mean
+    performance_df['delta'] = delta
     performance_df['%increase'] = rel_increase
-    
+    performance_df['sig_count'] = p_value_delta
+    performance_df['test_count'] = test_count
     
     return performance_df
         
-eff_reach_performance = agency_AB_performance(values=['Unique user click per impression'])
+brand_rec_performance = agency_AB_performance(values=['UU click pi',
+                                                      'UU view pi',
+                                                      'UU click pe',
+                                                      'UU view pe'])
 
-sales_performance = agency_AB_performance(values=['Post-view conversions per impression',
-                                                  'Post-click conversions per impression'])
+sales_performance = agency_AB_performance(values=['Conv pi',
+                                                  'Conv pe',
+                                                  'Post-view conv pi',
+                                                  'Post-click conv pi',
+                                                  'Post-view conv pe',
+                                                  'Post-click conv pe'])
 
-cost_efficiency_performance = agency_AB_performance(values=[
-                                                'Unique user view per impression '])
-
-
-    
 def plot_group_barchart(dataframe, title):
     fig, ax = plt.subplots(figsize=(6, 12))
     ind = np.arange(len(dataframe))                                   # the x locations for the groups
@@ -92,7 +114,7 @@ def plot_group_barchart(dataframe, title):
 #plot_group_barchart(dataframe=eff_reach_performance , title=' ')
     
 #fig, ax = plt.subplots(figsize=(14, 8))
-plt.scatter(x=data['campaign_duration'], y=data['Unique user click per impression'])
+#plt.scatter(x=data['campaign_duration'], y=data['UU click pi'])
 
-ax.set_title("Unique user click per impression per euro", fontsize=21)
-plt.xlabel("Campaign duration (days)", fontsize=15)
+#ax.set_title("Unique user click per impression per euro", fontsize=21)
+#plt.xlabel("Campaign duration (days)", fontsize=15)
