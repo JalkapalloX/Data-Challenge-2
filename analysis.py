9ec9684c77e3afc8813e1e395c0660534bfb00e0
jaymonv <47352670+jaymonv@users.noreply.github.com>
2019-03-26 11:12:38 +0100

    update

--- analysis.py
+++ analysis.py
@@ -114,15 +114,99 @@ def agency_AB_performance(values, data=data):
     return performance_df
 
 
-brand_rec_performance = agency_AB_performance(values=['UU click pi',
-                                                      'UU view pi'])
 
-sales_performance = agency_AB_performance(values=['Conv pi',
-                                                  'Conv pe',
+agency_AB_performance(values=['UU click pi', 'UU view pi']).to_csv("Agency_branding_performance.csv")
+
+agency_AB_performance(values=['Conv pi',
                                                   'Post-view conv pi',
-                                                  'Post-click conv pi',
-                                                  'Post-view conv pe',
-                                                  'Post-click conv pe'])
+                                                  'Post-click conv pi']).to_csv("Agency_sales_performance.csv")
+    
+
+def agency_AB_performance(values, 
+                          data=data,
+                          cost_efficiency=False):
+
+    a_mean = []
+    b_mean = []
+    delta = []
+    indicator = []
+    rel_increase = []
+    p_value_higher = []
+    p_value_lower = []
+    test_count = []
+    
+    for i in values:
+
+        data = data[data['buyer_bid'] > 0]
+        data.drop_duplicates(['campaign_group_id', 'operating_system', 
+                              'device_type', 'test_group'], 
+                              inplace=True)
+
+        global df
+        df = data.groupby(['campaign_group_id', 'operating_system', 'device_type', 'test_group'])[i].mean().unstack().dropna()
+        df2 = data.groupby(['campaign_group_id', 'operating_system', 'device_type', 'test_group'])['impressions'].sum().unstack().dropna()
+        
+        df = pd.concat([df, df2], axis=1)
+        df.columns = ['A', 'B', 'A_count', 'B_count']
+        
+        df = df[(df['A'] != 0) & (df['B'] != 0)]
+        
+        a_mean_series = df['A'] / df['A_count']
+        b_mean_series = df['B'] / df['B_count']
+        
+        df['diff'] = b_mean_series - a_mean_series
+        
+        indicator.append(i)
+        a_mean.append(np.mean(a_mean_series))
+        b_mean.append(np.mean(b_mean_series))
+        delta.append(np.mean(df['B']) - np.mean(df['A']))
+        rel_increase.append((np.mean(df['B']) - np.mean(df['A']))/abs(np.mean(df['A'])) * 100)
+        
+        # Z-test B > A
+        p1 = a_mean_series
+        p2 = b_mean_series
+        var1 = p1*(1-p1)
+        var2 = p2*(1-p2)
+
+        z = (p2-p1)/(var1/df['A_count'] + var2/df['B_count'])**0.5
+        pval = norm.cdf(z)
+        count = 0
+
+        for i in pval:
+            if i < 0.05:
+                count += 1
+
+        p_value_higher.append(count)
+        test_count.append(df['A'].count())
+        
+        # Z-test A > B
+        p1 = df['B']
+        p2 = df['A']
+
+        var1 = p1*(1-p1)
+        var2 = p2*(1-p2)
+
+        z = (p2-p1)/(var1/df['A_count'] + var2/df['B_count'])**0.5
+        pval = norm.cdf(z)
+        count = 0
+
+        for i in pval:
+            if i < 0.05:
+                count += 1
+
+        p_value_lower.append(count)
+
+    performance_df = pd.DataFrame()
+    performance_df['indicator'] = indicator
+    performance_df['A_mean'] = a_mean
+    performance_df['B_mean'] = b_mean
+    performance_df['delta'] = delta
+    performance_df['%increase'] = rel_increase
+    performance_df['sig_count_higher'] = p_value_higher
+    performance_df['sig_count_lower'] = p_value_lower
+    performance_df['test_count'] = test_count
+    
+    return performance_df
 
 def plot_group_barchart(dataframe, title):
     fig, ax = plt.subplots(figsize=(6, 12))
@@ -135,12 +219,3 @@ def plot_group_barchart(dataframe, title):
     ax.set_xticklabels(tuple(dataframe['indicator']), fontsize=14)
     #ax.legend((p1[0], p2[0]), ('Control algorithm', 'Bidwiser algorith'), fontsize=15)
     fig.savefig(fname=title)
-
-
-#plot_group_barchart(dataframe=eff_reach_performance , title=' ')
-    
-#fig, ax = plt.subplots(figsize=(14, 8))
-#plt.scatter(x=data['campaign_duration'], y=data['UU click pi'])
-
-#ax.set_title("Unique user click per impression per euro", fontsize=21)
-#plt.xlabel("Campaign duration (days)", fontsize=15)
